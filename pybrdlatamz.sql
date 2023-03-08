034c758e315921e5b74c86e3a9beb22d8775f819
unknown <gamd999@outlook.com>
2023-03-08 18:15:00 -0500

    Construccion de tablas de especies de aves y mamiferos presentes en territorios del Amazonas Colombia segun BD GBIF

--- pybrdlatamz.sql
+++ pybrdlatamz.sql
@@ -1,4 +1,12 @@
 -- Inicio del Proyecto Bird Latam Amazon con propositos ecoturisticos
 
+-- Socializacion de especies de aves (764 especies) por nombre cientifico y numero de registros GBIF presentes 
+-- en territorio del Deparatamento del Amazonas Colombia a la fecha de marzo del 2023
 SELECT species, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Aves'
 GROUP BY species ORDER BY rec_gbif DESC;
+
+-- Socializacion de especies de mamiferos (125 especies) por nombre cientifico y numero de registros GBIF presentes 
+-- en territorio del Deparatamento del Amazonas Colombia a la fecha de marzo del 2023
+SELECT species, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Mammalia'
+GROUP BY species ORDER BY rec_gbif DESC;
+
