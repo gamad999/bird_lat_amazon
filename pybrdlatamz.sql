3a5b2eb62e18f6cda997f8904189ddc8fab22e39
unknown <gamd999@outlook.com>
2023-03-08 18:06:53 -0500

    Inicio de desarrollo sql base de datos biodiversidad del Departamento del Amazonas colombia

--- pybrdlatamz.sql
+++ pybrdlatamz.sql
@@ -1,2 +1,4 @@
 -- Inicio del Proyecto Bird Latam Amazon con propositos ecoturisticos
 
+SELECT species, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Aves'
+GROUP BY species ORDER BY rec_gbif DESC;
