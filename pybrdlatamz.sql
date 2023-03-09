e56300ce6e2e4409cedbcc12b966a3aeb8471384
unknown <gamd999@outlook.com>
2023-03-08 22:44:27 -0500

    Ajuste tabla de especies de avifauna en el Departamento del Amazonas Colombia con inclusion de familias biologicas

--- pybrdlatamz.sql
+++ pybrdlatamz.sql
@@ -2,8 +2,8 @@
 
 -- Socializacion de especies de aves (764 especies) por nombre cientifico y numero de registros GBIF presentes 
 -- en territorio del Deparatamento del Amazonas Colombia a la fecha de marzo del 2023
-SELECT species, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Aves'
-GROUP BY species ORDER BY rec_gbif DESC;
+SELECT species, family, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Aves'
+GROUP BY species, family ORDER BY rec_gbif DESC;
 
 -- Socializacion de especies de mamiferos (125 especies) por nombre cientifico y numero de registros GBIF presentes 
 -- en territorio del Deparatamento del Amazonas Colombia a la fecha de marzo del 2023
