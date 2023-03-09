2e448a90c2d7fa5f84f1927e6a416b12bed25332
unknown <gamd999@outlook.com>
2023-03-08 20:23:27 -0500

    Ejemplo traduccion al chino mandarin de campos familias biologicas

--- pybrdlatamz.sql
+++ pybrdlatamz.sql
@@ -29,6 +29,39 @@ GROUP BY scientific ORDER BY rec_gbif DESC;
 SELECT family, COUNT(DISTINCT species) AS species, COUNT(DISTINCT id) AS rec_gbif 
 FROM gbif_amazonas WHERE class = 'Aves' GROUP BY family ORDER BY species DESC, rec_gbif;
 
+-- Creacion campo nombre de familia en chino mandarin
+ALTER TABLE gbif_amazonas ADD COLUMN family_zh varchar(90);
+
+-- Creacion de campo nombre de familia pinyin en chino mandarin
+ALTER TABLE gbif_amazonas ADD COLUMN family_pinyin varchar(90);
+
+-- Creacion de campo nombre comun en chino mandarin
+ALTER TABLE gbif_amazonas ADD COLUMN name_zh varchar(150);
+
+-- Consulta de familias
+SELECT species, COUNT(DISTINCT id) AS rec_gbif
+FROM gbif_amazonas WHERE family = 'Thraupidae' GROUP BY species ORDER BY rec_gbif DESC;
+
+-------------------  Traducciones al chino mandarin -------------------------------------
+
+-- Traduccion de nombre de familias biologicas al chino mandarin
+UPDATE gbif_amazonas SET family_zh = '鹭科' WHERE family = 'Ardeidae';
+
+-- Traduccion fonetica nombre de familias al chino mandarin
+UPDATE gbif_amazonas SET family_pinyin = 'Lù kē' WHERE family = 'Ardeidae';
+
+-- Nombre comun de familias biologicas en español
+ALTER TABLE gbif_amazonas ADD COLUMN nombre_familia varchar(80);
+
+-- Actualizacion nombre de familia biologica en español
+UPDATE gbif_amazonas SET nombre_familia = 'Garzas' 
+WHERE family = 'Ardeidae';
+
+SELECT family_zh, family_pinyin, nombre_familia, COUNT(DISTINCT id) AS rec
+FROM gbif_amazonas WHERE family = 'Ardeidae' GROUP BY family_zh, family_pinyin, nombre_familia
+ORDER BY rec;
+
+
 
 
 
