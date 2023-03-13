89a92c155f40fdc60c309e1737dcf8b78239e84d
unknown <gamd999@outlook.com>
2023-03-13 17:34:06 -0500

    Diagnostico de biodiversidad de abejas en Departamento de Amazonas

--- pybrdlatamz.sql
+++ pybrdlatamz.sql
@@ -29,6 +29,16 @@ GROUP BY scientific ORDER BY rec_gbif DESC;
 SELECT scientific, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas WHERE class = 'Insecta'
 GROUP BY scientific ORDER BY rec_gbif DESC;
 
+
+-- Socializacion de especies de abejas (38 especies) por nombre cientifico y numero de registros GBIF presentes 
+-- en territorio del Departamento del Amazonas Colombia a la fecha de marzo del 2023
+SELECT scientific, family, COUNT(DISTINCT id) AS rec_gbif FROM gbif_amazonas 
+WHERE family = 'Apidae' OR family = 'Colletidae' OR family = 'Halictidae' OR 
+family = 'Megachilidae'
+GROUP BY scientific, family ORDER BY rec_gbif DESC;
+
+
+
 -- Consulta de familias de aves por numero de especies y registros GBIF
 SELECT family, COUNT(DISTINCT species) AS species, COUNT(DISTINCT id) AS rec_gbif 
 FROM gbif_amazonas WHERE class = 'Aves' GROUP BY family ORDER BY species DESC, rec_gbif;
