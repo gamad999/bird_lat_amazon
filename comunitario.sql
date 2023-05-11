b2e1a2c45e7f65a6c3f330505d1c2ea4d81e9f16
gamad999 <gamard199@gmail.com>
2023-05-11 12:58:28 -0500

    Inicio de diagnostico preliminar comunitario Amazonas

--- comunitario.sql
+++ comunitario.sql
@@ -6,3 +6,8 @@
 ALTER TABLE resguardos_indigenas_amazonas ADD COLUMN area_has double precision;
 
 UPDATE resguardos_indigenas_amazonas SET area_has = (SELECT ST_Area(geom)/10000.0);
+
+--- Inicio diagnostico preliminar de componente comunitario Amazonas Colombia
+
+SELECT nombre_res AS reguardo, matricula_, pueblo, municipio, area_has
+FROM resguardos_indigenas_amazonas;
